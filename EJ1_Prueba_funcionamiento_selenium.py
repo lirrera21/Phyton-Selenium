b7b781816b9cae76659bf0c1d49c32f930254ac3
lirrera@gmail.com <lirrera@gmail.com>
2022-08-20 11:08:07 -0300

    primer commit

---
+++ EJ1_Prueba_funcionamiento_selenium.py
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path=r"C:\Users\lirrera\Documents\Seleniume\explorer_driver\chromedriver.exe")
+driver.get("http://www.python.org")
+=======
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path=r"C:\Users\lirrera\Documents\Seleniume\explorer_driver\chromedriver.exe")
+driver.get("http://www.python.org")
+>>>>>>> 27875135178212997b2f00ecd3806bc7f50b1df5
+driver.close()
